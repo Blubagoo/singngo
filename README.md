d56e5471648a360aa756483942f1cbc4f0783bf3
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2020-06-25 13:19:13 -0500

    fixed javascript and some styling issues.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
     <header>
       <h1 align="center"> Sing 'N Go</h1>
       <p align="center">Use Sing'N Go for to look up lyrics and showtimes of your favorite artists.</p>
-    <header
+    </header>
     <main>
       <p>The Sing 'N Go app uses two different api's Lyrics OVH and TicketMaster Events. The app can be found <a href="https://blubagoo.github.io/singngo/" target="_blank">here</a>.
       </p>
--- js/app.js
+++ js/app.js
@@ -1,103 +1,95 @@
-var apiConfigG = {
-				  dataType: "JSON",
-				  method: "GET",
-				  error: () =>
-					console.log(arguments)
-			 	  };
-
-//listen for form submit, taking values and running function to send api calls
-function listenForSubmit() {
+//helper variables
+let apiConfigG = {
+	dataType: "JSON",
+	method: "GET",
+	error: (err) => console.log('error', err)
+}
+let eventAPIKey = "foT0mqx1A21ZxgjogM48Svp5vNF7gbgy"
+//event listener for submitting the form
+const listenForSubmit = () => {
 	$('#search-form').submit('#search-button', function(e) {
-		
 		e.preventDefault();
-				
+		//get values for the artist and title from form		
 		let artist = $('#artist').val();
 		let title = $('#title').val();
-		
+		//reset the form for UX/UI
 		$('#artist').val('');
 		$('#title').val('');
-
+		//call for artist and lyrics
 		getLyrics(artist, title);
 		getArtist(artist);
-		
+		//swing in animation for lyrics and tickets
 		$('#lyric-bg').show(800, "swing");
 		$('#event-bg').show(800, "swing");
-		});
+	});	
 }
-
-
-
-//send lyric api data
-function getLyrics(artist, title) {
+const getLyrics = (artist, title) => {
 	const settings = {
-						url:`https://api.lyrics.ovh/v1/${artist}/${title}`,
-						success: data => displayLyrics(data, artist, title)
-					  };
-
+		url:`https://api.lyrics.ovh/v1/${artist}/${title}`,
+		success: data => renderLyrics(data, artist, title)
+	};
+	// combine the standard api object with the new settings 
+	let configApi = Object.assign({}, apiConfigG, settings);
+	$.ajax(configApi);
+}
+//call api for artist id. You need to provide an artist name, usually exact match.
+const getArtistID = (artist) => {
+	const settings = {
+		url: "https://app.ticketmaster.com/discovery/v2/attractions",
+		data: {
+			keyword: artist,
+			apikey: eventAPIKey
+		},
+		async: true,
+		success: data => getEvents(data._embedded.attractions[0].id;)
+	};
+	// combine the standard api object with the new settings 
+	let configApi = Object.assign({}, apiConfigG, settings);
+	$.ajax(configApi);
+}
+const getEvents = (artistId) => {
+	const settings = {
+		url: "https://app.ticketmaster.com/discovery/v2/events",
+		data: {
+				attractionId: artistId,
+				apikey: eventAPIKey
+			   },
+		async: true,
+		success: data => renderEvents(data)
+	};
+	// combine the standard api object with the new settings 
 	var configApi = Object.assign({}, apiConfigG, settings);
 	$.ajax(configApi);
 }
-//call to api with artist search, redirecting back the artist id
-function getArtist(artist) {
-	const settings = {
-						url: "https://app.ticketmaster.com/discovery/v2/attractions",
-						data: {
-								keyword: artist,
-								apikey: "foT0mqx1A21ZxgjogM48Svp5vNF7gbgy"
-					  		  },
-						async: true,
-						success: data => redirectToApi(data)
-					  };
-
-	var configApi = Object.assign({}, apiConfigG, settings);
-	$.ajax(configApi);
-}
-//artist id collected and response made
-function redirectToApi(data) {
-	const artistId = data._embedded.attractions[0].id;
-	getEvents(artistId);
-}
-//api call with artist id to ticketmaster
-function getEvents(artistId) {
-	const settings = {
-						url: "https://app.ticketmaster.com/discovery/v2/events",
-						data: {
-								attractionId: artistId,
-								apikey: "foT0mqx1A21ZxgjogM48Svp5vNF7gbgy"
-							   },
-						async: true,
-						success: data => displayEvents(data)
-					 };
-
-	var configApi = Object.assign({}, apiConfigG, settings);
-	
-	$.ajax(configApi);
-}
-//display events for artist
-function displayEvents(data) {
+//render the artist events
+const renderEvents = data => {
 	if(data.page.totalElements === 0) {
 		const noEvent = noEvents();
 		$('#event-area').append(noEvent);
 	} else {
-		const event = data._embedded.events.map(item=>  `<div class="events">
-														<p class="date">${item.dates.start.localDate}</p>
-														<a href="${item.url}" target="_blank"><img src="${item.images[0].url}" alt="${item.name}" name="${item.id}" class="images"></a>
-														<p class="event-info"><b>${item.name}</b><br></p>
-														<p class="event-info">${item._embedded.venues[0].city.name},<br>${item._embedded.venues[0].country.name}</p>
-														</div>`)
+		const event = data._embedded.events.map(item=>  `
+			<div class="events">
+				<p class="date">${item.dates.start.localDate}</p>
+				<a href="${item.url}" target="_blank"><img src="${item.images[0].url}" alt="${item.name}" name="${item.id}" class="images"></a>
+				<p class="event-info"><b>${item.name}</b><br></p>
+				<p class="event-info">${item._embedded.venues[0].city.name},<br>${item._embedded.venues[0].country.name}</p>
+			</div>
+		`)
 		$('#event-area').append(event)
 	};	
 }
-//parse the lyrics from api
-function displayLyrics(data, artist, title) {
+//parse the lyrics
+const renderLyrics = (data, artist, title) => {
+	//lyrics come in a single string of text, so we split by linebreak and then enclose a p element wrapped around the line
 	let splitLyric 	 = data.lyrics.split(/\n/).map(item => `<p class="lyric-info">${item}</p>`);
 	//create header for artist
-	var header = `<div class="lyrics">
-				<h2 class="artist-name">Lyrics for ${artist}!</h2>
-				<h3 class="title-name">${title}
-				<div id="lyric-area">
-				</div>
-				</div>`;
+	let header = `
+		<div class="lyrics">
+			<h2 class="artist-name">Lyrics for ${artist}!</h2>
+			<h3 class="title-name">${title}
+			<div id="lyric-area" />
+		</div>
+	`;
 	
 	$('#lyric-bg').html(header)
 	$('#lyric-area').html(splitLyric);
